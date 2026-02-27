fd675cf15c617631fb3f183fe82f1f1374d32d57
Pablo Moreno <pfmjmoreno6a@gmail.com>
2026-02-26 20:41:42 -0500

    Se agregó los links de paginacion

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,9 +1,6 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- 👇 Esto debe estar -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+@vite('resources/css/app.css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
 <h1 class="text-center mt-5 align-self-center">CRUD de Productos</h1>
 
@@ -65,3 +62,7 @@
     @endforelse
 </tbody>
 </table>
+
+<div class="mt-3 d-flex justify-content-center">
+    {{ $productos->links() }}
+</div>
